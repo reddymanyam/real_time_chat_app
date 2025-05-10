083c83bdd5e15223b205634012569f8145040399
reddymanyam <reddappareddy18@gmail.com>
2025-05-10 12:16:41 +0530

    upd: toggling sun and moon icon for themes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
-import { CircleUser, LayoutDashboard, MessagesSquare, Moon, Settings } from 'lucide-react'
+import { CircleUser, LayoutDashboard, MessagesSquare, Moon, Settings, Sun } from 'lucide-react'
 import Avatarr from '../reusablecomponents/Avatarr';
+import { useState } from 'react';
 
 const iconStyle = "p-4 rounded-lg text-gray-600 hover:text-indigo-600 hover:bg-gray-200 cursor-pointer transition-colors";
 
 const Sidebar = (
     // { isChatProfileOpen }
 ) => {
+
+    const [lightMode, setLightMode] = useState(true);
 
     return (
         <div className="flex h-full w-full flex-row overflow-x-auto overflow-y-hidden lg:flex-col lg:justify-between lg:overflow-hidden  px-4 lg:px-2 py-2 z-40">
@@ -24,9 +27,15 @@ const Sidebar = (
                 <div className={iconStyle}>
                     <Settings strokeWidth={1.5} />
                 </div>
-                <div className={iconStyle}>
-                    <Moon strokeWidth={1.5} />
-                </div>
+                {lightMode ? (
+                    <div className={iconStyle} onClick={() => setLightMode(!lightMode)}>
+                        <Sun strokeWidth={1.5} />
+                    </div>
+                ) : (
+                    <div className={iconStyle} onClick={() => setLightMode(!lightMode)}>
+                        <Moon strokeWidth={1.5} />
+                    </div>
+                )}
                 <div className={iconStyle}>
                     <Avatarr src="https://docs.material-tailwind.com/img/face-2.jpg" fallback="PP" />
                 </div>
